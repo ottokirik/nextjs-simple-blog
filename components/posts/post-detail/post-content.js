2548177ca74182d81ada7018018f5e9a02fbd159
Petrov Kirill <ottokirik@gmail.com>
2021-05-28 01:06:25 +0300

    Реализовано: компонент PostDeatil, добавлена поддержка markdown в проект

---
+++ components/posts/post-detail/post-content.js
@@ -0,0 +1,26 @@
+import ReactMarkdown from 'react-markdown';
+
+import classes from 'components/posts/post-detail/post-content.module.css';
+import { PostHeader } from './post-header';
+
+const DUMMY_POST = {
+  slug: 'getting-started-with-nextjs',
+  title: 'Getting Started with NextJS',
+  image: 'getting-started-nextjs.png',
+  date: '2022-02-10',
+  content:
+    '# Dolor nostrum sit commodi dignissimos temporibus dolores id saepe.',
+};
+
+const PostContent = () => {
+  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+
+  return (
+    <article className={classes.content}>
+      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+    </article>
+  );
+};
+
+export { PostContent };
